32294e2267c07af877d5c7c6811dcad91a327475
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-27 23:12:33 +0100

    Restrict get books path with auth headers

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const validate = require('../../middleware/validate');
+const { authenticate } = require('../../middleware/authenticate');
 const Users = require('../../models/users');
 const auth = require('./auth');
 const books = require('./books');
 
-router.post('/register', validate(Users.registerSchema), auth.register)
-router.post('/login', validate(Users.loginSchema), auth.login)
-router.get('/books', books.getBooks)
+router.post('/register', validate(Users.registerSchema), auth.register);
+router.post('/login', validate(Users.loginSchema), auth.login);
+router.get('/books', authenticate, books.getBooks);
 
 module.exports = router;

d07d118aa00c9b698a515c240bd52bce1c09d4e4
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-27 20:35:54 +0100

    Create database for books, users and reviews
    - connect to postgres via knex
    - create seperate db for production, testing and development

---
+++ database/migrations/20190727201231_users.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', user => {
+    user.increments();
+
+    user.string('first_name', 255).notNullable();
+    user.string('last_name', 255).notNullable();
+    user
+      .string('email', 255)
+      .notNullable()
+      .unique();
+    user.string('password', 255).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};

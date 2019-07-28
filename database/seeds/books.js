e7a2836282dcefb243043ef49e91d9dfed02c376
Nabeelah <35764602+NabeelahY@users.noreply.github.com>
2019-07-28 22:03:23 +0100

    Update books.js

--- database/seeds/books.js
+++ database/seeds/books.js
@@ -4,6 +4,7 @@ exports.seed = function(knex) {
     .then(function() {
       return knex('books').insert([
         {
+          id: 1,
           isbn: 9781593275846,
           title: 'Eloquent JavaScript, Second Edition',
           subtitle: 'A Modern Introduction to Programming',
@@ -13,6 +14,7 @@ exports.seed = function(knex) {
           description: 'JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.'
         },
         {
+          id: 2,
           isbn: 9781449331818,
           title: 'Learning JavaScript Design Patterns',
           subtitle: "A JavaScript and jQuery Developer's Guide",
@@ -22,6 +24,7 @@ exports.seed = function(knex) {
           description: "With Learning JavaScript Design Patterns, you'll learn how to write beautiful, structured, and maintainable JavaScript by applying classical and modern design patterns to the language. If you want to keep your code efficient, more manageable, and up-to-date with the latest best practices, this book is for you."
         },
         {
+          id: 3,
           isbn: 9781449365035,
           title: 'Speaking JavaScript',
           subtitle: 'An In-Depth Guide for Programmers',
@@ -31,6 +34,7 @@ exports.seed = function(knex) {
           description: 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position.'
         },
         {
+          id: 4,
           isbn: 9781491950296,
           title: 'Programming JavaScript Applications',
           subtitle: 'Robust Web Architecture with Node, HTML5, and Modern JS Libraries',
@@ -40,6 +44,7 @@ exports.seed = function(knex) {
           description: "Take advantage of JavaScript's power to build robust web-scale or enterprise applications that are easy to extend and maintain. By applying the design patterns outlined in this practical book, experienced JavaScript developers will learn how to write flexible and resilient code that's easier-yes, easier-to work with as your code base grows."
         },
         {
+          id: 5,
           isbn: 9781593277574,
           title: 'Understanding ECMAScript 6',
           subtitle: 'The Definitive Guide for JavaScript Developers',
@@ -49,6 +54,7 @@ exports.seed = function(knex) {
           description: 'ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript.'
         },
         {
+          id: 6,
           isbn: 9781491904244,
           title: "You Don't Know JS",
           subtitle: 'ES6 & Beyond',
@@ -58,6 +64,7 @@ exports.seed = function(knex) {
           description: 'No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the "You Don’t Know JS" series, this compact guide focuses on new features available in ECMAScript 6 (ES6), the latest version of the standard upon which JavaScript is built.'
         },
         {
+          id: 7,
           isbn: 9781449325862,
           title: 'Git Pocket Guide',
           subtitle: 'A Working Introduction',
@@ -67,6 +74,7 @@ exports.seed = function(knex) {
           description: 'This pocket guide is the perfect on-the-job companion to Git, the distributed version control system. It provides a compact, readable introduction to Git for new users, as well as a reference to common commands and procedures for those of you with Git experience.'
         },
         {
+          id: 9,
           isbn: 9781449337711,
           title: 'Designing Evolvable Web APIs with ASP.NET',
           subtitle: 'Harnessing the Power of the Web',

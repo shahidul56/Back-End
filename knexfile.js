bec385b3b7a0065c822a400d9822d35a2a5164b2
Nabeelah <35764602+NabeelahY@users.noreply.github.com>
2019-07-28 16:49:14 +0100

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection:`postgres://qzdmeftrkmnjty:fe02507650e31b1440d0d3c2fe25f242a3943e1fd38f09acc5e6ef7574f97a3d@ec2-54-243-193-59.compute-1.amazonaws.com:5432/d4ulvnibfnb44k`,
+    connection:'postgres://qzdmeftrkmnjty:fe02507650e31b1440d0d3c2fe25f242a3943e1fd38f09acc5e6ef7574f97a3d@ec2-54-243-193-59.compute-1.amazonaws.com:5432/d4ulvnibfnb44k',
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',

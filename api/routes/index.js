1efd81b0c50902e442bda37a7f63e4a133c08872
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-29 11:45:15 +0100

    Create endpoint to get user by id

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,12 +5,17 @@ const Users = require('../../models/users');
 const Books = require('../../models/books');
 const Review = require('../../models/reviews');
 const auth = require('./auth');
+const users = require('./users');
 const books = require('./books');
 const reviews = require('./review');
 
 // Auth
 router.post('/register', validate(Users.registerSchema), auth.register);
 router.post('/login', validate(Users.loginSchema), auth.login);
+
+// Users
+
+router.get('/users/:id', authenticate, users.getUsersByID);
 
 // Books
 router.get('/books', authenticate, books.getBooks);

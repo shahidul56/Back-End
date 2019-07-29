18115a1654d9ec4084fdc09c47e40bf8616fbaf0
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-29 10:43:13 +0100

    Implement endpoint to edit review

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,12 +3,16 @@ const validate = require('../../middleware/validate');
 const { authenticate } = require('../../middleware/authenticate');
 const Users = require('../../models/users');
 const Books = require('../../models/books');
+const Review = require('../../models/reviews');
 const auth = require('./auth');
 const books = require('./books');
 const reviews = require('./review');
 
+// Auth
 router.post('/register', validate(Users.registerSchema), auth.register);
 router.post('/login', validate(Users.loginSchema), auth.login);
+
+// Books
 router.get('/books', authenticate, books.getBooks);
 router.get('/books/:id', authenticate, books.getBook);
 router.post(
@@ -18,6 +22,9 @@ router.post(
   reviews.addReview
 );
 router.delete('/books/:id', authenticate, books.deleteBookById);
+
+// Reviews
 router.get('/reviews/:id', authenticate, reviews.getReviewById);
 router.get('/user/:id/reviews', authenticate, reviews.getUserReviews);
+router.put('/reviews/:id', validate(Review.reviewSchema), reviews.editReview);
 module.exports = router;
--- models/reviews.js
+++ models/reviews.js
@@ -1,3 +1,4 @@
+const Joi = require('@hapi/joi');
 const db = require('../database/dbConfig');
 
 module.exports = {
@@ -6,5 +7,25 @@ module.exports = {
   },
   findUsersReviews: id => {
     return db('reviews').where('reviewer', id);
+  },
+  updateReview: (id, review) => {
+    return db('reviews')
+      .where({ id })
+      .update(review)
+      .returning('*');
+  },
+  reviewSchema: review => {
+    const schema = Joi.object().keys({
+      review: Joi.string()
+        .min(2)
+        .max(255)
+        .required(),
+      reviewer: Joi.number().required(),
+      ratings: Joi.number()
+        .required()
+        .min(1)
+        .max(5)
+    });
+    return Joi.validate(review, schema);
   }
 };

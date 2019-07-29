922c9f1c6b5bd717ca0be2c2ffe8e0bb8d834db8
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-29 11:54:36 +0100

    Refactor code

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,9 +5,9 @@ module.exports = {
     try {
       const { id } = req.params;
       const [user] = await Users.findUserBy({ id });
-      delete user.password;
+
       user
-        ? res.status(200).json(user)
+        ? (delete user.password, res.status(200).json(user))
         : res.status(404).json({ error: 'User not found' });
     } catch (error) {
       res.status(500).json({ error: error.message });

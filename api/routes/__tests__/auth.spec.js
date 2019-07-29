7d2737392fff382edd9e4a65294798d55fc3b38c
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-29 16:50:56 +0100

    Test register and login endpoints

---
+++ api/routes/__tests__/auth.spec.js
@@ -0,0 +1,53 @@
+const db = require('../../../database/dbConfig');
+const server = require('../../server');
+const request = require('supertest')(server);
+
+beforeAll(async () => {
+    await db.raw(
+      'TRUNCATE TABLE users, books, reviews CASCADE'
+    );
+  });
+
+describe('Authentication', () => {
+  it('Users are able to sign up', () => {
+    return request
+      .post('/api/register')
+      .send({
+        first_name: 'Pamela',
+        last_name: 'Robert',
+        email: 'pamela@gmail.com',
+        password: 'password'
+      })
+      .expect(201)
+      .expect('Content-Type', /json/);
+  });
+  it('Users are able to login', async () => {
+    return request
+      .post('/api/register')
+      .send({
+        first_name: 'Pamela',
+        last_name: 'Robert',
+        email: 'pamela@gmail.com',
+        password: 'password'
+      })
+      .then(res => {
+        return request
+          .post('/api/login')
+          .send({
+            email: 'pamela@gmail.com',
+            password: 'password'
+          })
+          .expect(200);
+      });
+  });
+
+  it('Gives error message when username or password is not inputed', () => {
+    return request
+      .post('/api/register')
+      .send({
+        first_name: 'Pamela',
+        email: 'pamela@gmail.com'
+      })
+      .expect(422);
+  });
+});
